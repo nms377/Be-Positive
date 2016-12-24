8a8601aed122d0c33eea90e1497ca2a54dca3b7b
nms377 <nms377@nyu.edu>
2016-12-24 11:45:07 -1000

    Used a lot of if statements to provide patients with alternative compatible blood types if OG blood type is not in stock.

---
+++ js/Be-Positive.js
@@ -0,0 +1,119 @@
+BloodType = {
+  
+  AB_POS : "AB_POS",
+  AB_NEG : "AB_NEG",
+  A_POS  : "A_POS",
+  A_NEG  : "A_NEG",
+  B_POS  : "B_POS",
+  B_NEG  : "B_NEG",
+  O_POS  : "O_POS",
+  O_NEG  : "O_NEG"
+
+};
+
+BloodTransfusionRules = {
+  
+  /**
+   * Set the simulation speed.
+   * @type {Number} : Valid values between 1 and 200
+   */
+  simulation_speed : 200,
+
+  /**
+   * returns BloodType, or false to give no BloodType
+   * 
+   * @name receive_patient
+   * @param {Bank} blood_inventory
+   * @param {Patient} patient
+   * @returns {BloodType or false}
+   *
+   * Patient properties {
+   *   gender : String, (MALE,FEMALE)
+   *   blood_type : String (BloodType)
+   * }
+   * 
+   * Bank properties {
+   *   AB_POS : Integer,
+   *   AB_NEG : Integer,
+   *   A_POS  : Integer,
+   *   A_NEG  : Integer,
+   *   B_POS  : Integer,
+   *   B_NEG  : Integer,
+   *   O_POS  : Integer,
+   *   O_NEG  : Integer
+   * }
+   * 
+   */
+
+  receive_patient : function (blood_inventory, patient) {
+    
+    // give a random blood type to anyone
+    // very bad idea!
+    // return [
+    //   BloodType.AB_POS,
+    //   BloodType.AB_NEG,
+    //   BloodType.A_POS,
+    //   BloodType.A_NEG
+    // ][Math.floor(Math.random()*4)];
+
+    if(BloodType.hasOwnProperty(patient.blood_type)){
+      if(patient.blood_type === BloodType.AB_POS){
+        if(blood_inventory.AB_POS > 0){
+          return BloodType.AB_POS;
+          }else if(blood_inventory.A_POS > 0){
+            return BloodType.A_POS;
+            }else if(blood_inventory.B_POS > 0){
+              return BloodType.B_POS;
+              }else if(blood_inventory.O_POS > 0){
+                return BloodType.O_POS;
+                }else{return null;
+                  }
+      }else if(patient.blood_type === BloodType.AB_NEG){
+        if(blood_inventory.AB_NEG >0){
+          return BloodType.AB_NEG;
+          }else if(blood_inventory.A_NEG > 0){
+            return BloodType.A_NEG;
+            }else if(blood_inventory.B_NEG >0){
+              return BloodType.B_NEG;
+              }else if(blood_inventory.O_NEG > 0){
+                return BloodType.O_NEG;
+                }else{return null;
+                  }
+      }else if(patient.blood_type === BloodType.A_POS){
+        if(blood_inventory.A_POS > 0){
+          return BloodType.A_POS;
+          }else if(blood_inventory.O_POS > 0){
+            return BloodType.O_POS;
+            }
+      }else if(patient.blood_type === BloodType.A_NEG){
+        if(blood_inventory.A_NEG >0){
+          return BloodType.A_NEG;
+          }else if(blood_inventory.O_NEG > 0){
+            return BloodType.O_NEG;
+            }
+      }else if(patient.blood_type === BloodType.B_POS){
+        if(blood_inventory.B_POS > 0){
+          return BloodType.B_POS;
+          }else if(blood_inventory.O_POS > 0){
+            return BloodType.O_POS;
+            }
+      }else if(patient.blood_type === BloodType.B_NEG){
+        if(blood_inventory.B_NEG > 0){
+          return BloodType.B_NEG;
+        }else if(blood_inventory.O_NEG > 0){
+          return BloodType.O_NEG;
+          }
+      }else if(patient.blood_type === BloodType.O_POS){
+        if(blood_inventory.O_POS > 0){
+          return BloodType.O_POS;
+        }
+      }else if(patient.blood_type === BloodType.O_NEG){
+        if(blood_inventory.O_NEG > 0){
+          return BloodType.O_NEG;
+        }else{return null;
+        }
+      }
+    }
+  }
+
+};
